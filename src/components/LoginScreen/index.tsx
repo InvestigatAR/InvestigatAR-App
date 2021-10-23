b3ed9722c4ff29d7cede6e671eea62155ed30106
vivekvjk <vivekvjk2020@gmail.com>
2021-10-23 00:59:25 -0400

    combine signup screen

--- src/components/LoginScreen/index.tsx
+++ src/components/LoginScreen/index.tsx
@@ -1,22 +1,32 @@
-import {Button, Text, View} from 'react-native';
-import React from 'react';
+import {Button, Text, View, Alert} from 'react-native';
+import React, {useState} from 'react';
 import { connect } from "react-redux";
 import { mapDispatchToProps, mapStateToProps } from "../LaunchScreen";
 
 const LoginScreen = (props: any) => {
 
-    // use state hook to refer to input field for getting information of whats typed
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>INVESTIGATAR Login Screen</Text>
-      <Button
-        title="Login"
-        // change TabScreen to LoginScreen
-        onPress={() => props.navigation.navigate('TabScreen')}
-      />
+    const [username, setUserName] = useState<String | null>(null);
+    const [password, setPassword] = useState<String | null>(null);
 
-    </View>
-  );
+    const signup = async () => {
+        if (username && password) {
+            
+        } else {
+            Alert.alert("Error", "Missing Fields");
+        }
+    };
+    // use state hook to refer to input field for getting information of whats typed
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>INVESTIGATAR Login Screen</Text>
+        <Button
+            title="Login"
+            // change TabScreen to LoginScreen
+            onPress={() => props.navigation.navigate('TabScreen')}
+        />
+
+        </View>
+    );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
---
+++ src/components/Shared/button.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from 'react';
+import {Dimensions, View, Text, StyleSheet} from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
+
+const {height, width} = Dimensions.get('screen')
+interface Props {
+    title: string;
+    onPress: () => void
+}
+
+const Button : FC <Props> = (props) => {
+    return (
+        <TouchableOpacity style={styles.container} onPress={props.onPress}>
+            <Text style={styles.text}>{props.title}</Text>
+        </TouchableOpacity>
+    )
+}
+export default Button
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor : '#000',
+        alignItems : 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        padding: 10,
+        borderRadius: 8,
+        marginVertical: 10
+    },
+    text : {
+        color: '#fff'
+    }
+})
