40b571be6160a93640ba08f34d2d9aa274e56b58
Skyline-9 <luo.richard@gmail.com>
2021-10-23 21:53:49 -0400

    :sparkles: Testing tfjs

--- src/components/ScannerScreen/index.tsx
+++ src/components/ScannerScreen/index.tsx
@@ -1,97 +1,142 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { mapDispatchToProps, mapStateToProps } from "../LaunchScreen";
-import { View, Alert, ImageBackground, ImagePickerIOS, StyleSheet, Text } from "react-native";
-import { RNCamera } from "react-native-tflite-camera";
+import { View, Alert, StyleSheet, Text, Image } from "react-native";
+import { RNCamera } from "react-native-camera";
 import GenButton from "../Shared/genButton";
-import Tflite from "tflite-react-native";
-import ImagePicker from "react-native-image-picker";
-import { Platform } from "react-native";
+
+import RNFS from "react-native-fs";
+import ImagePicker from 'react-native-image-picker';
+import { CameraOptions, ImageLibraryOptions } from "react-native-image-picker";
+
+import * as jpeg from "jpeg-js";
+
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 
 const ScannerScreen = (props: any) => {
 
     const [popupShown, setPopupShown] = useState<any>(false);
     const [camera, setCamera] = useState<any>(undefined);
-    const [inference, setInference] = useState<any>();
-    const [source, setSource] = useState<any>();
-    const [imageHeight, setImageHeight] = useState<any>();
-    const [imageWidth, setImageWidth] = useState<any>();
-    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-    let tflite = new Tflite();
+    //Tensorflow stuff
+    const [isTfReady, setIsTfReady] = useState<boolean>(false);
+    const [isModelReady, setIsModelReady] = useState<boolean>(false);
+    const [predictions, setPredictions] = useState<any>();
+    const [imageURI, setImageURI] = useState<any>();
+    const [model, setModel] = useState<any>();
 
-    const onSelectImage = () => {
-        tflite = new Tflite();
-        console.log("Logging tflite ", tflite);
+    /* AI Object Detection */
+    useEffect(() => {
+        async function loadTf() {
+            await tf.ready();
+            setIsTfReady(true);
 
-        if (!isLoaded) {
-            tflite.loadModel({
-                    model: "../../../models/ssd_mobilenet.tflite",
-                    labels: "../../../models/ssd_mobilenet.txt"
-                },
-                (err, res) => {
-                    if (err) console.log(err);
-                    else setInference(res);
-                });
-            setIsLoaded(true);
+            setModel(await cocossd.load());
+            setIsModelReady(true);
         }
 
-        const options: any = {
-            title: "Select Avatar",
-            customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
-            storageOptions: {
-                skipBackup: true,
-                path: "images"
-            }
+        loadTf();
+    }, []);
+
+    const imageToTensor = (rawImageData) => {
+        const TO_UINT8ARRAY: any = true;
+        const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
+
+        // Drop the alpha channel info for mobilenet
+        const buffer = new Uint8Array(width * height * 3);
+
+        let offset = 0; // offset into original data
+        for (let i = 0; i < buffer.length; i += 3) {
+            buffer[i] = data[offset];
+            buffer[i + 1] = data[offset + 1];
+            buffer[i + 2] = data[offset + 2];
+
+            offset += 4;
+        }
+
+        return tf.tensor3d(buffer, [height, width, 3]);
+    };
+
+    const detectObjects = async () => {
+        try {
+            const imageAssetPath = imageURI;
+
+            console.log(imageAssetPath.uri);
+
+            let imgB64;
+            RNFS.readFile(imageAssetPath.uri, "base64")
+                .then(res => {
+                    imgB64 = res;
+                    console.log(res);
+                });
+
+            const imgBuffer = tf.util.encodeString(imgB64, "base64").buffer;
+            const raw = new Uint8Array(imgBuffer);
+            const imageTensor = imageToTensor(raw);
+            console.log("imageTensor: ", imageTensor);
+            const predictions = await model.detect(imageTensor);
+
+            setPredictions({ predictions: predictions });
+
+            console.log("----------- predictions: ", predictions);
+
+        } catch (error) {
+            console.log("Exception Error: ", error);
+        }
+    };
+
+    const selectImage = async () => {
+        const options: ImageLibraryOptions & CameraOptions = {
+            mediaType: "photo",
+            videoQuality: "high",
+            quality: 1,
+            maxWidth: 0,
+            maxHeight: 0,
+            includeBase64: true,
+            cameraType: "back",
+            selectionLimit: 1,
+            saveToPhotos: false,
+            durationLimit: 0
         };
 
-        //Load image from image library
-        ImagePicker.launchImageLibrary(options, (response: any) => {
+        ImagePicker.launchImageLibrary(options, (response:any) => {
+            console.log('Response = ', response);
+
             if (response.didCancel) {
-                console.log("User cancelled image picker");
-            } else if (response.errorCode) {
-                console.log("ImagePicker Error: ", response.errorCode);
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
             } else {
-                const path = Platform.OS === "ios" ? response.uri : "file://" + response.path;
+                const source = { uri: response.uri };
 
-                const w = response.width;
-                const h = response.height;
-
-                setImageWidth(w);
-                setImageHeight(h);
-                setSource({ uri: path });
-
-                tflite.detectObjectOnImage({
-                        path,
-                        threshold: 0.2,
-                        numResultsPerClass: 1
-                    },
-                    (err, res) => {
-                        if (err) console.log(err);
-                        else setInference({ recognitions: res });
-                    });
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+                // alert(JSON.stringify(response));s
+                console.log('response', JSON.stringify(response));
+                setImageURI(response.uri);
             }
         });
     };
 
-    const renderResults = () => {
-        onSelectImage();
+    const renderPrediction = (prediction, index) => {
+        const pclass = prediction.class;
+        const score = prediction.score;
+        const x = prediction.bbox[0];
+        const y = prediction.bbox[1];
+        const w = prediction.bbox[2];
+        const h = prediction.bbox[3];
 
-        return inference.map((res, id) => {
-            const left = res["rect"]["x"] * imageWidth;
-            const top = res["rect"]["y"] * imageHeight;
-            const width = res["rect"]["w"] * imageWidth;
-            const height = res["rect"]["h"] * imageHeight;
-            return (
-                <View key={id} style={[styles.box, { top, left, width, height }]}>
-                    <Text style={{ color: "white", backgroundColor: "blue" }}>
-                        {res["detectedClass"] + " " + (res["confidenceInClass"] * 100).toFixed(0) + "%"}
-                    </Text>
-                </View>
-            );
-        });
+        return (
+            <Text key={index} style={styles.text}>
+                Prediction: {pclass} {", "} Probability: {score} {", "} Bbox: {x} {", "} {y} {", "} {w} {", "} {h}
+            </Text>
+        );
     };
 
+
+    /* QR Code Scanning */
     const showAlert = (productId) => {
         if (popupShown) {
             return;
@@ -161,52 +206,98 @@ const ScannerScreen = (props: any) => {
                 onGoogleVisionBarcodesDetected={barcodeRecognized}
 
             />
-            <GenButton title={"Recognize object"} onPress={renderResults} />
+            <GenButton title={"Recognize object"} onPress={selectImage} />
+            <View style={styles.predictionWrapper}>
+                {isModelReady && imageURI && (
+                    <Text style={styles.text}>
+                        Predictions: {predictions ? "" : "Detecting..."}
+                    </Text>
+                )}
+
+                {isModelReady &&
+                predictions &&
+                predictions.map((p, index) => renderPrediction(p, index))}
+            </View>
         </View>
     );
 };
 
+export default connect(mapStateToProps, mapDispatchToProps)(ScannerScreen);
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white"
+        backgroundColor: "#171f24"
     },
-    imageContainer: {
-        borderColor: "blue",
-        borderRadius: 5,
-        alignItems: "center"
+    welcomeContainer: {
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 20
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: "contain",
+        marginTop: 3,
+        marginLeft: -10
+    },
+    contentContainer: {
+        paddingTop: 30
+    },
+    loadingContainer: {
+        marginTop: 80,
+        justifyContent: "center"
     },
     text: {
-        color: "blue"
+        color: "#ffffff",
+        fontSize: 16
     },
-    button: {
-        width: 200,
-        backgroundColor: "blue",
-        borderRadius: 10,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        marginBottom: 10
+    loadingModelContainer: {
+        flexDirection: "row",
+        marginTop: 10
     },
-    buttonText: {
-        color: "white",
-        fontSize: 15
-    },
-    box: {
-        position: "absolute",
+    imageWrapper: {
+        width: 280,
+        height: 280,
+        padding: 10,
         borderColor: "blue",
-        borderWidth: 2
+        borderWidth: 5,
+        borderStyle: "dashed",
+        marginTop: 40,
+        marginBottom: 10,
+        position: "relative",
+        justifyContent: "center",
+        alignItems: "center"
     },
-    boxes: {
+    imageContainer: {
+        width: 250,
+        height: 250,
         position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 0
+        top: 10,
+        left: 10,
+        bottom: 10,
+        right: 10
+    },
+    predictionWrapper: {
+        height: 100,
+        width: "100%",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    transparentText: {
+        color: "#ffffff",
+        opacity: 0.7
+    },
+    footer: {
+        marginTop: 40
+    },
+    poweredBy: {
+        fontSize: 20,
+        color: "#e69e34",
+        marginBottom: 6
+    },
+    tfLogo: {
+        width: 125,
+        height: 70
     }
 });
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(ScannerScreen);
