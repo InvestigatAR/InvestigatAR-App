e44d225018442937cd26948834aa07d9af7e0c9c
rdeepak2002 <rdeepak2002@gmail.com>
2021-10-24 01:07:06 -0400

    asdf

--- src/service/utils.ts
+++ src/service/utils.ts
@@ -5,17 +5,25 @@ export const addProductToStorage = (prod: any) => {
     let curProdArr: Array<any> = [];
 
     if (res !== undefined && res !== null) {
-      curProdArr = res;
+      curProdArr = JSON.parse(res);
     }
 
-    // only add product if it is not already in storage array
-    if (
-      !curProdArr.some(product => {
-        return product.id === prod.id;
-      })
-    ) {
-      curProdArr.push(prod);
+    for (let i = 0; i < curProdArr.length; i++) {
+      if (curProdArr[i].id === prod.id) {
+        curProdArr.splice(i, 1);
+      }
     }
+    //
+    // // only add product if it is not already in storage array
+    // if (
+    //   !curProdArr.some(product => {
+    //     return product.id === prod.id;
+    //   })
+    // ) {
+    //   curProdArr.push(prod);
+    // }
+
+    curProdArr.push(prod);
 
     AsyncStorage.setItem('productList', JSON.stringify(curProdArr));
   });
